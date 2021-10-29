d84b9c327d3511098f1339c1f8927ac094835426
Boris Aguilar <baguilar@innovare.es>
2021-10-29 13:11:09 -0600

    Improvements in log viewing

--- components/logViewer.js
+++ components/logViewer.js
@@ -23,7 +23,7 @@ const useLogData = (
   token,
   setStatusName,
   setPercentage,
-  setErrorState
+  setColorState
 ) => {
   if (uuid === undefined) {
     return {
@@ -40,18 +40,19 @@ const useLogData = (
   useEffect(() => {
     if (data) {
         if (data.status) {
-          if (data.status.error_status) {
+          if (data.status.error_status || error) {
             setStatusName("Error");
-            setErrorState(true);
-          } else if (data.status.output_init) {
+            setColorState("red");
+          } else if (data.status.output_init && !data.status.completed) {
             setPercentage("20%");
             setStatusName("Applying");
-          } else if (data.status.output_apply) {
+          } else if (data.status.output_apply && !data.status.completed) {
             setPercentage("40%");
             setStatusName("Applying");
           } else if (data.status.completed) {
             setPercentage("100%");
             setStatusName("Finished");
+            setColorState("green");
           } else {
             setPercentage("10%");
             setStatusName("Initializing");
@@ -69,13 +70,13 @@ const useLogData = (
 export default function LogViewer({ uuid, token }) {
   const [statusName, setStatusName] = useState("Warming up");
   const [percentage, setPercentage] = useState("0%");
-  const [errorState, setErrorState] = useState(false);
+  const [colorState, setColorState] = useState("purple");
   const { log, isLoading, isError } = useLogData(
     uuid,
     token,
     setStatusName,
     setPercentage,
-    setErrorState
+    setColorState
   );
   if (!uuid) return <></>;
   return (
@@ -86,21 +87,21 @@ export default function LogViewer({ uuid, token }) {
             <div className="relative pt-3 border p-2 rounded-t shadow">
               <div className="flex mb-2 items-center justify-between">
                 <div>
-                  <span className={`text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full ${errorState?"text-red-600 bg-red-200":"text-purple-600 bg-purple-200"}`}>
+                  <span className={`text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-${colorState}-600 bg-${colorState}-200`}>
                     {statusName}
                   </span>
                 </div>
                 <div className="text-sm">UUID: {uuid}</div>
                 <div className="text-right">
-                  <span className={`text-xs font-semibold inline-block ${errorState?"text-red-600":"text-purple-600"}`}>
+                  <span className={`text-xs font-semibold inline-block text-${colorState}-600`}>
                     {percentage}
                   </span>
                 </div>
               </div>
-              <div className={`overflow-hidden h-2 mb-4 text-xs flex rounded ${errorState?"bg-red-200":"bg-purple-200"}`}>
+              <div className={`overflow-hidden h-2 mb-4 text-xs flex rounded bg-${colorState}-200`}>
                 <div
                   style={{ width: percentage }}
-                  className={`shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center ${errorState?"bg-red-500":"bg-purple-500"}`}
+                  className={`shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-${colorState}-500`}
                 ></div>
               </div>
             </div>
@@ -111,12 +112,18 @@ export default function LogViewer({ uuid, token }) {
           {isLoading && <p>Please wait...</p>}
           {log && log.status && (
             <>
+              <pre className="text-green-600 bg-green-200 hidden">Color output green</pre>
+              <pre className="text-green-600 bg-green-500 hidden">Color output green A</pre>
+              <pre className="text-red-600 bg-red-200 hidden">Color output red</pre>
+              <pre className="text-red-600 bg-red-500 hidden">Color output red A</pre>
+              <pre className="text-purple-600 bg-purple-200 hidden">Color output purple</pre>
+              <pre className="text-purple-600 bg-purple-500 hidden">Color output purple A</pre>
               <pre>Init Output</pre>
               <pre>
                 <Ansi>{log.status.output_init || "-- No output --"}</Ansi>
               </pre>
               <pre>Apply Output</pre>
-              <pre>{log.status.otuput_apply || "-- No output --"}</pre>
+              <pre><Ansi>{log.status.output_apply || "-- No output --"}</Ansi></pre>
               {log.status.error && (
                   <pre className="text-red-300">Error: <Ansi>{log.status.error}</Ansi></pre>
               )}
